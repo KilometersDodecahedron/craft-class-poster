aa18feb403d9298ba7ee46ffd54670c5a04c7bee
Miles Cohn <milesselim@icloud.com>
2023-04-30 15:28:02 -0400

    removed some unneeded dependancies

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,11 @@
 const router = require("express").Router()
 const craftClassRoutes = require("./craft-class.js")
 const tagRoutes = require("./tags")
-const imgBBRoutes = require("./imgbb")
 const categoryRoutes = require("./category")
 const mediaSliderRoutes = require("./media-sliders")
 
 router.use("/craft_class", craftClassRoutes)
 router.use("/tag", tagRoutes)
-router.use("/imgbb", imgBBRoutes)
 router.use("/category", categoryRoutes)
 router.use("/media_slider", mediaSliderRoutes)
 
